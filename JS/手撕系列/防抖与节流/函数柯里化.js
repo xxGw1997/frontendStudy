0da225b14097ced5bedc00225fe84da95ed57b95
xxgw <540040588@qq.com>
2020-10-29 21:41:22 +0800

    函数柯里化

--- JS/手撕系列/防抖与节流/函数柯里化.js
+++ JS/手撕系列/防抖与节流/函数柯里化.js
@@ -1,15 +1,21 @@
-let currying = (fn,...args) => {
-  if(fn.length>args.length){
+/**
+ * 思想就是将参数逐渐传递下去,到最后将参数都收集完后后再统一一起处理
+ */
+
+let currying = (fn, ...args) => {
+  console.log('fn.length:', fn.length)
+  console.log('args.length:', args.length)
+  if (fn.length > args.length) {
     return (...arguments) => {
-      return currying(fn,...args,...arguments)
+      return currying(fn, ...args, ...arguments)
     }
-  }else{
+  } else {
     return fn(...args)
   }
 }
 
-let addSum = (a, b, c) => a + b + c
+let addSum = (a, b, c, d) => a + b + c + d
 let add = currying(addSum)
-console.log(add(1)(2)(3))
-console.log(add(1, 2)(3))
-console.log(add(1, 2, 3))
+console.log(add(1)(2)(3, 4))
+// console.log(add(1, 2)(3))
+// console.log(add(1, 2, 3))
