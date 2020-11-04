2f5787e0e8206190b547e540952cf7b983c53d95
xxgw <540040588@qq.com>
2020-11-04 23:30:49 +0800

    多个promise问题

--- JS/手撕系列/Promise-code/MyPromise.js
+++ JS/手撕系列/Promise-code/MyPromise.js
@@ -7,16 +7,24 @@ class MyPromise {
     this.status = PENDING
     this.value = undefined
     this.reason = undefined
+
+    this.onFulfilledCallbacks = []
+    this.onRejectedCallbacks = []
+
     const resolve = value => {
       if (this.status === PENDING) {
         this.status = FULFILLED
         this.value = value
+        //发布
+        this.onFulfilledCallbacks.forEach(fn => fn())
       }
     }
     const reject = reason => {
       if (this.status === PENDING) {
         this.status = REJECTED
         this.reason = reason
+        //发布
+        this.onRejectedCallbacks.forEach(fn => fn())
       }
     }
     try {
@@ -32,6 +40,16 @@ class MyPromise {
     }
     if (this.status === REJECTED) {
       onRejected(this.reason)
+    }
+
+    if (this.status === PENDING) {
+      //订阅
+      this.onFulfilledCallbacks.push(() => {
+        onFulfilled(this.value)
+      })
+      this.onRejectedCallbacks.push(() => {
+        onRejected(this.reason)
+      })
     }
   }
 }
--- JS/手撕系列/Promise-code/index.js
+++ JS/手撕系列/Promise-code/index.js
@@ -3,7 +3,10 @@ const MyPromise = require('./MyPromise')
 let promise = new MyPromise((resolve, reject) => {
   // resolve('success')
   // reject('error')
-  throw new Error('Exception Error')
+  // throw new Error('Exception Error')
+  setTimeout(() => {
+    resolve('succuss')
+  }, 2000);
 })
 
 promise.then(value => {
