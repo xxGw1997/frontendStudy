d0b3df5b4a64be9c04de33182a31aa7ea921d981
xxgw <540040588@qq.com>
2020-11-05 22:59:37 +0800

    手写promise A+ 规范

--- JS/手撕系列/Promise-code/MyPromise.js
+++ JS/手撕系列/Promise-code/MyPromise.js
@@ -2,6 +2,39 @@ const PENDING = 'PENDING',
   FULFILLED = 'FULFILLED',
   REJECTED = 'REJECTED'
 
+
+function resolvePromise(promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    return reject(new TypeError('promise 循环'))
+  }
+  let called = false
+
+  if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
+    try {
+      let then = x.then
+      if (typeof then === 'function') {
+        then.call(x, (y) => {
+          if (called) return
+          called = true
+          resolvePromise(promise2, y, resolve, reject)
+        }, (r) => {
+          if (called) return
+          called = true
+          reject(r)
+        })
+      } else {
+        resolve(x)
+      }
+    } catch (e) {
+      if (called) return
+      called = true
+      reject(e)
+    }
+  } else {
+    resolve(x)
+  }
+}
+
 class MyPromise {
   constructor(excutor) {
     this.status = PENDING
@@ -35,23 +68,59 @@ class MyPromise {
   }
 
   then(onFulfilled, onRejected) {
-    if (this.status === FULFILLED) {
-      onFulfilled(this.value)
-    }
-    if (this.status === REJECTED) {
-      onRejected(this.reason)
-    }
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
+    onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason }
 
-    if (this.status === PENDING) {
-      //订阅
-      this.onFulfilledCallbacks.push(() => {
-        onFulfilled(this.value)
-      })
-      this.onRejectedCallbacks.push(() => {
-        onRejected(this.reason)
-      })
-    }
+    let promise2 = new MyPromise((resolve, reject) => {
+      if (this.status === FULFILLED) {
+        setTimeout(() => {
+          try {
+            let x = onFulfilled(this.value)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        }, 0);
+      }
+      if (this.status === REJECTED) {
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        }, 0);
+      }
+
+      if (this.status === PENDING) {
+        //订阅
+        this.onFulfilledCallbacks.push(() => {
+          try {
+            let x = onFulfilled(this.value)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        })
+        this.onRejectedCallbacks.push(() => {
+          try {
+            let x = onRejected(this.reason)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        })
+      }
+    })
+    return promise2
+  }
+
+  catch(errorCallback) {
+    return this.then(null, errorCallback)
   }
 }
+
+
 
 module.exports = MyPromise
