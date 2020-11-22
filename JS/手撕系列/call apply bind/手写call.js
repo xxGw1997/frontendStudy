98c648af2e8a66b6687b66955bc3c819f1f61c43
xxGw1997 <540040588@qq.com>
2020-11-22 15:04:23 +0800

    手写call

---
+++ JS/手撕系列/call apply bind/手写call.js
@@ -0,0 +1,28 @@
+Function.prototype.myCall = function(context = globalThis) {
+    // 设置 fn 为调用 myCall 的方法
+    context.fn = this;
+  
+    // 获取剩余参数
+    const otherArg = Array.from(arguments).slice(1);
+  
+    // 调用这个方法，将剩余参数传递进去
+    context.fn(otherArg);
+  
+    // 将这个方法的执行结果传给 result
+    let result = context.fn();
+  
+    // 删除这个变量
+    delete context.fn;
+  
+    // 返回 result 结果
+    return result;
+  };
+  
+  this.a = 1;
+  
+  const fn = function() {
+    this.a = 2;
+    console.log(this.a);
+  }
+  
+  fn.myCall(fn);
