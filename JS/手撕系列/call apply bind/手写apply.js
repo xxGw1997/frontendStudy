12e22d49afcf1df8fce371bf7affb26a4838ea0b
xxGw1997 <540040588@qq.com>
2020-11-22 19:18:39 +0800

    apply

---
+++ JS/手撕系列/call apply bind/手写apply.js
@@ -0,0 +1,33 @@
+// Function.prototype.myApply = function(context = globalThis){
+//     let args = [...arguments]
+//     context.fn = this
+//     context.fn(args)
+//     let res = context.fn()
+//     delete context.fn
+//     return res
+// }
+
+
+Function.prototype.myApply = function(context = globalThis){
+    console.log('this:',this)
+    console.log('context:',context)
+    context.fn = this
+    const args = [...arguments]
+    const res =  args.length>1?context.fn(args):context.fn()
+    delete context.fn
+    return res
+}
+
+const a = {
+    name:'a',
+    say:function(){
+        console.log(this.name)
+    }
+}
+
+const b = {
+    name:'b',
+
+}
+
+a.say.myApply(b)
