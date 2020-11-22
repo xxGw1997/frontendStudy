c08b351adbf16adb0d11cfcd46fc7af0cc1edbfe
xxGw1997 <540040588@qq.com>
2020-11-22 20:45:57 +0800

    bind

--- JS/手撕系列/call apply bind/手写call.js
+++ JS/手撕系列/call apply bind/手写call.js
@@ -1,22 +1,34 @@
-Function.prototype.myCall = function(context = globalThis) {
-    // 设置 fn 为调用 myCall 的方法
-    context.fn = this;
+// Function.prototype.myCall = function(context = globalThis) {
+//     // 设置 fn 为调用 myCall 的方法
+//     context.fn = this;
   
-    // 获取剩余参数
-    const otherArg = Array.from(arguments).slice(1);
+//     // 获取剩余参数
+//     const otherArg = Array.from(arguments).slice(1);
   
-    // 调用这个方法，将剩余参数传递进去
-    context.fn(otherArg);
+//     // 调用这个方法，将剩余参数传递进去
+//     context.fn(otherArg);
   
-    // 将这个方法的执行结果传给 result
-    let result = context.fn();
+//     // 将这个方法的执行结果传给 result
+//     let result = context.fn();
   
-    // 删除这个变量
-    delete context.fn;
+//     // 删除这个变量
+//     delete context.fn;
   
-    // 返回 result 结果
-    return result;
-  };
+//     // 返回 result 结果
+//     return result;
+//   };
+
+
+Function.prototype.myCall = function(context = globalThis){
+    let args =   Array.from(arguments).slice(1)
+    context.fn = this
+    let res = args.length>0?context.fn(args):context.fn()
+    delete context.fn
+    return res
+}
+
+
+
   
   this.a = 1;
   
